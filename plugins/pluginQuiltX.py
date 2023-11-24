8b6b36d65a71386f526f9865406af0cc533a7565
Bernard Kwok <kwokcb@gmail.com>
2023-11-24 14:50:43 -0500

    Add in glTF load.

--- plugins/pluginQuiltX.py
+++ plugins/pluginQuiltX.py
@@ -15,13 +15,28 @@ import MaterialX as mx
 logger = logging.getLogger(__name__)
 
 class GltfQuiltix(quiltix.QuiltiXWindow):
+    '''
+    QuiltiX window with glTF import/export functionality added
+    '''
     def __init__(self, load_style_sheet=True, load_shaderball=True, load_default_graph=True):
         super().__init__(load_style_sheet=True, load_shaderball=True, load_default_graph=True)
-        export_gltf = QAction("Export as Gltf...", self)
-        export_gltf.triggered.connect(self.export_gtlf_triggered)
+
+        self.file_menu.addSeparator()
+
+        # Export menu item
+        export_gltf = QAction("Export to glTF...", self)
+        export_gltf.triggered.connect(self.export_gltf_triggered)
         self.file_menu.addAction(export_gltf)
 
-    def export_gtlf_triggered(self):
+        # Import menu item
+        import_gltf = QAction("Import from glTF...", self)
+        import_gltf.triggered.connect(self.import_gltf_triggered)
+        self.file_menu.addAction(import_gltf)
+
+    def import_gltf_triggered(self):
+        '''
+        Import a glTF file into the current graph.
+        '''
         start_path = self.mx_selection_path
         if not start_path:
             start_path = self.geometry_selection_path
@@ -30,7 +45,49 @@ class GltfQuiltix(quiltix.QuiltiXWindow):
             start_path = os.path.join(ROOT, "resources", "materials")
 
         path = self.request_filepath(
-            title="Save glTF file", start_path=start_path, file_filter="MaterialX files (*.gltf)", mode="save"
+            title="Load glTF file", start_path=start_path, file_filter="glTF files (*.gltf)", mode="open",
+        )
+
+        options = core.GLTF2MtlxOptions()
+        options['createAssignments'] = False   
+        options['addAllInputs'] = False
+        gltf2MtlxReader = core.GLTF2MtlxReader()
+        gltf2MtlxReader.setOptions(options)
+        doc = gltf2MtlxReader.convert(path)
+        success = True
+        if not doc:
+            success  = False
+            logger.error('Error converting glTF file to MaterialX file')
+        else:
+            success , err = doc.validate()
+            if not success:
+                logger.error(err)
+            else:
+                # Load material file, stipped of any library references
+                # - Q: Should we attempt to replace usage of "channels" with explicit extracts here 
+                # as OpenUSD does not handle this properly. TBD.
+                # - Note: we must strip out the library references as QuiltiX tries to load them in
+                # as includes instea of stripping them out.
+                docString = core.Util.writeMaterialXDocString(doc)
+                doc = mx.createDocument()
+                mx.readFromXmlString(doc, docString)
+                
+                self.mx_selection_path = path
+                self.qx_node_graph.load_graph_from_mx_doc(doc, path)
+
+    def export_gltf_triggered(self):
+        '''
+        Export the current graph to a glTF file.
+        '''
+        start_path = self.mx_selection_path
+        if not start_path:
+            start_path = self.geometry_selection_path
+
+        if not start_path:
+            start_path = os.path.join(ROOT, "resources", "materials")
+
+        path = self.request_filepath(
+            title="Save glTF file", start_path=start_path, file_filter="glTF files (*.gltf)", mode="save",
         )
 
         options = core.MTLX2GLTFOptions()
@@ -62,16 +119,19 @@ class GltfQuiltix(quiltix.QuiltiXWindow):
                 f.write(gltf_string)
                 logger.info(f"Wrote .gltf file to {path}")
 
+            # Package binary file
             binaryFileName = str(path)
-            binaryFileName = binaryFileName.replace('.gltf', '.glb')
-            print('- Packaging GLB file...')
+            binaryFileName = binaryFileName.replace('.gltf', '.glb')            
+            logger.debug('- Packaging GLB file...')
             mtlx2glTFWriter = core.MTLX2GLTFWriter()
+            mtlx2glTFWriter.setOptions(options)
             saved, images, buffers = mtlx2glTFWriter.packageGLTF(path, binaryFileName)
-            print('- Save GLB file:' + binaryFileName + '. Status:' + str(saved))
+            logger.debug('- Save GLB file:' + binaryFileName + '. Status:' + str(saved))
             for image in images:
-                print('  - Embedded image: %s' % image)
+                logger.debug('  - Embedded image: ' + image)
             for buffer in buffers:
-                print('  - Embedded buffer: %s' % buffer)
+                logger.debug('  - Embedded buffer: ' + buffer)
+            logger.debug('- Packaging GLB file... finished.')
 
         except Exception as e:
             logger.error(e)
@@ -92,25 +152,25 @@ class GltfQuiltix(quiltix.QuiltiXWindow):
 
         # Perform shader translation if needed
         translatedCount = mtlx2glTFWriter.translateShaders(doc)
-        print('- Translated %d shaders.' % translatedCount)
+        logger.debug('- Translated shaders.' + str(translatedCount))
 
         # Perform baking if needed
         if translatedCount > 0:
-            print('- Baking start {')
+            logger.debug('- Baking start {')
             bakeResolution = 1024
             bakedFileName = options['bakeFileName']
             #if options['bakeResolution']:
             #    bakeResolution = options['bakeResolution']
             mtlx2glTFWriter.bakeTextures(doc, False, bakeResolution, bakeResolution, False, 
                                         False, False, bakedFileName)
-            print('  - Baked textures to: ', bakedFileName)
+            logger.debug('  - Baked textures to: ' + bakedFileName)
             doc, libFiles = core.Util.createMaterialXDoc()
             mx.readFromXmlFile(doc, bakedFileName, options['searchPath'])
             remappedUris = core.Util.makeFilePathsRelative(doc, bakedFileName)
             for uri in remappedUris:
-                print('  - Remapped URI: %s to %s' % (uri[0], uri[1]))
+                logger.debug('  - Remapped URI: ' + uri[0] + ' to ' + uri[1])
                 core.Util.writeMaterialXDoc(doc, bakedFileName)
-            print('- Baking end.')
+            logger.debug('- Baking end.')
 
         gltfString = mtlx2glTFWriter.convert(doc)
 
